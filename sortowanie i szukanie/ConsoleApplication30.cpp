b1685c2b0c0f925517da6f4324ba79645d8982f1
Oliwier Popielarczyk <tj.cyber@wp.pl>
2022-01-25 16:23:48 +0100

    usunieto smieci i dodano sortowanie z szukaniem

---
+++ sortowanie i szukanie/ConsoleApplication30.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include "sortowanie.h"
+using namespace std;
+int main()
+{
+	int szukana;
+	sortowanie obiekt;
+	cout << "czego chcesz szukac smiertelniku\n";
+
+	obiekt.quicksort(0, obiekt.elementy.size() - 1);
+	obiekt.wypisz_vector();
+	cin >> szukana;
+	int wynik = 0;
+	wynik = obiekt.szukajposortowane(0, obiekt.elementy.size() - 1, szukana);
+	cout << "ilosc wywolan " << obiekt.temppp << endl;
+	if (wynik == -1)
+	{
+		cout << "nic nie znaleziono\n";
+	}
+	else
+	{
+		cout << "znaleziono wartosc na pozycji " << wynik;
+	}
+
+
+	sortowanie obiekt1;
+	cout << endl << "czego chcesz szukac smiertelniku\n";
+	cin >> szukana;
+	wynik = obiekt1.szukaj(0, szukana);
+	cout << "ilosc wywolan " << obiekt1.temppp << endl;
+	if (wynik == -1)
+	{
+		cout << "nic nie znaleziono\n";
+	}
+	else
+	{
+		cout << "znaleziono wartosc na pozycji " << wynik;
+	}
+
+}
--- sortowanie i szukanie/sortowanie.h
+++ sortowanie i szukanie/sortowanie.h
@@ -9,11 +9,14 @@ class sortowanie
 public:
 	sortowanie();
 	~sortowanie();
-	int temp, temp1;
+	int temp, temp1; 	int temppp;
 	vector <int> elementy;
 	void sortowanie_babelki();
 	void sortowanie_wstawianie();
 	void wypisz_vector();
+	void sortowanie_wybor();
+	void quicksort(int left, int right);
+	void sortowanie_scalanie(int iP, int iK);
+	int szukaj(int p, int k);
+	int szukajposortowane(int lewy, int prawy, int szukana);
 };
-
-
