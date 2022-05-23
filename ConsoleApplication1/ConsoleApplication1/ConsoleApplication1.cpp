1495c83ba9b3898664f3bab104dfd8c3e3e0f90a
olipop210 <71080644+olipop210@users.noreply.github.com>
2022-05-23 16:30:45 +0200

    toz to jest algorytmy biblioteka

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+	string tekst = "Ala ma kota ";
+	cout << tekst << endl;
+	vector<char>znaki;
+	cout << "jakie znaki chcesz usunac/zamienic?\n";
+	cout << "& zamyka wpisywanie\n";
+	while (true)
+	{
+		char temp;
+		cin >> temp;
+		if (temp == '&') 
+		{ 
+			break; 
+		}
+		else
+		{
+			znaki.push_back(temp);
+		}
+	}
+	cout << "na jaki znak chcesz to zamienic?\n";
+	char znak; cin >> znak;
+
+
+	auto lambda = [&znaki](char znak)
+	{
+		for (int i = 0; i < znaki.size(); i++)
+		{
+			if (znak == znaki[i])
+			{
+				return 1;
+			}
+		}
+		return 0;
+	};
+
+
+
+	auto  a = remove_if(tekst.begin(), tekst.end(), lambda);
+	tekst.erase(a, tekst.end());
+	//replace_if(tekst.begin(), tekst.end(), lambda, znak);
+	std::cout << tekst;
+
+}
+
+
